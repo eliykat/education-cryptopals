4daa67825c49e28c355ef11b89ab3701e35f5d34
Thomas Rittson <thomas.rittson@connect.qut.edu.au>
2020-03-18 10:08:02 +1000

    Set 1 Challenge 6 - fully implemented but not returning a useful result

--- Program.cs
+++ Program.cs
@@ -11,10 +11,129 @@ namespace Cryptopals
     {
         static void Main(string[] args)
         {
-            string string1 = "this is a test",
-                string2 = "wokka wokka!!!";
+            try
+            {
+                using (StreamReader sr = new StreamReader("/Users/tom/Projects/Cryptopals/6.txt"))
+                {
 
-            Console.WriteLine(hemmingDistance(Encoding.UTF8.GetBytes(string1), Encoding.UTF8.GetBytes(string2)));
+                    string ciphertext = sr.ReadToEnd(),
+                        plaintext;
+
+                    byte[] cipherbytes = new byte[ciphertext.Length],
+                        key;
+
+                    cipherbytes = Encoding.UTF8.GetBytes(ciphertext);
+
+                    key = breakVigenere(cipherbytes);
+                    plaintext = Encoding.UTF8.GetString(xorByteArrays(cipherbytes, key));
+
+
+                    Console.WriteLine("Key is {0}", Encoding.UTF8.GetString(key));
+                    Console.WriteLine("Plaintext is {0}", plaintext);
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error - file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        static byte[] breakVigenere(byte[] ciphertext)
+        {
+            int keysize = breakVigenereKeysize(ciphertext);
+            byte[] key = new byte[keysize];
+
+            int blocksInFile = ciphertext.Length / keysize;
+
+            // transposedBytes is a 2D byte array.
+            // Each row refers to one character in the key.
+            // Each column refers to a block of length keysize in the ciphertext (of which there are blocksInFile).
+            // The effect is to collate all chars that align with each char of the key in each row.
+            byte[][] transposedBytes = new byte[keysize][];
+
+            for (int i = 0; i<transposedBytes.Length; ++i)
+            {
+                transposedBytes[i] = new byte[blocksInFile];
+            }
+
+            for (int blockIndex = 0; blockIndex<blocksInFile; blockIndex++)
+            {
+                byte[] block = new byte[keysize];
+                Array.Copy(ciphertext, keysize * blockIndex, block, 0, keysize);
+
+                for (int item = 0; item<keysize; item++)
+                {
+                    transposedBytes[item][blockIndex] = block[item];
+                }
+            }
+
+            // Debug only
+            //Console.WriteLine("Transposed chars:");
+            //for (int i = 0; i<keysize; ++i)
+            //{
+            //    Console.WriteLine(i + ": " + Encoding.UTF8.GetString(transposedBytes[i]));
+            //}
+
+            for (int i = 0; i<keysize; ++i)
+            {
+                KeyValuePair<byte[], string> bruteForced = bruteForceSingleBitXOR(transposedBytes[i]);
+
+                key[i] = bruteForced.Key[0];
+            }
+
+            return key;
+        }
+
+        static int breakVigenereKeysize(byte[] ciphertext)
+        {
+
+            int guess = 0;
+            double highScore = 100;
+
+            // Try keysizes from 4 - 40
+            for (int keysize = 4; keysize < 41; keysize++)
+            {
+
+                byte[] block1 = new byte[keysize],
+                    block2 = new byte[keysize],
+                    block3 = new byte[keysize],
+                    block4 = new byte[keysize];
+
+                Array.Copy(ciphertext, block1, keysize);
+                Array.Copy(ciphertext, keysize, block2, 0, keysize);
+                Array.Copy(ciphertext, keysize * 2, block3, 0, keysize);
+                Array.Copy(ciphertext, keysize * 3, block4, 0, keysize);
+
+                double distance = (double)(hemmingDistance(block1, block2) + hemmingDistance(block3, block4)) / 2;
+                double normalised = distance / keysize;
+
+                // This is for debugging if required
+                //Console.WriteLine("Keysize {0}: Distance between {1} and {2} is {3}; distance between {4} and {5} is {6}; average is {7} and normalised is {8}",
+                //    keysize,
+                //    Encoding.UTF8.GetString(block1),
+                //    Encoding.UTF8.GetString(block2),
+                //    hemmingDistance(block1, block2),
+                //    Encoding.UTF8.GetString(block3),
+                //    Encoding.UTF8.GetString(block4),
+                //    hemmingDistance(block3, block4),
+                //    distance,
+                //    normalised);
+
+                //Console.WriteLine("For comparison: without averaging, based on first two blocks, it would be {0}",
+                //    (double)hemmingDistance(block1, block2) / keysize);
+
+                if (normalised < highScore)
+                {
+                    guess = keysize;
+                    highScore = normalised;
+                }
+            }
+
+            Console.WriteLine("Best guess at keylength: {0}", guess);
+
+            return guess;
         }
 
         static int hemmingDistance(byte[] barray1, byte[] barray2)
@@ -134,26 +253,38 @@ namespace Cryptopals
             }
 
             return plainText;
-
         }
 
-        static string bruteForceSingleBitXOR(byte[] cipherBytes)
+        static KeyValuePair<byte[], string> bruteForceSingleBitXOR(byte[] cipherBytes)
         {
             // Tries a range of single-bit keys against XOR'd ciphertext
-            // and returns the most likely plaintext based on frequency analysis
+            // and returns a key-value pair of key:plaintext based on frequency analysis
 
-            List<string> testPlainText = new List<string>();
+            List<string> testPlainTextList = new List<string>();
 
-            // Construct array of test plain texts
-            for (int i = 0; i < 127; i++)
+            Dictionary<string, byte[]> testPlainTextWithKeys = new Dictionary<string, byte[]>();
+
+            string plainText;
+            byte[] key;
+
+            // Constructs:
+            // 1. A list of test plain texts using every possible key - for the frequencyAnalysis function
+            // 2. A dictionary of the plain texts associated with their keys - so that we can identify the key
+            for (int i = 32; i < 127; i++)
             {
                 byte[] testKey = { Convert.ToByte(i) };
+                string testPlainText = System.Text.Encoding.UTF8.GetString(xorByteArrays(cipherBytes, testKey));
 
-                testPlainText.Add(System.Text.Encoding.UTF8.GetString(xorByteArrays(cipherBytes, testKey)));
+                testPlainTextList.Add(testPlainText);
+                testPlainTextWithKeys.Add(testPlainText, testKey);
+
             }
 
-            return frequencyAnalysis(testPlainText);
+            plainText = frequencyAnalysis(testPlainTextList);
 
+            testPlainTextWithKeys.TryGetValue(plainText, out key);
+
+            return new KeyValuePair<byte[], string>(key, plainText);
         }
     }
 }
